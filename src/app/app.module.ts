ead420fb30b4981a8075353a54fc7069968fa72e
puyz <puyzsoft@gmail.com>
2023-04-20 18:25:29 +0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule
